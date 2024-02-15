b55bade35d028c7991fd2e7f59261bf206858efe
Projkeen Theone <Projkeen Theone@PROJKEEN>
2024-02-15 19:55:20 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<Order> GetOrder() //done
+        {  
+            var order = await _db.Orders.AsNoTracking().ToListAsync();
+            var maxQuantity = order.OrderByDescending(p => p.Price).ThenByDescending(q=>q.Quantity).FirstOrDefault();
+            return maxQuantity;
+        }
+
+        public async Task<List<Order>> GetOrders() //done
+        {
+            var orders = await _db.Orders.ToListAsync();
+            var ordersOver10 = orders.Where(q => q.Quantity > 10).ToList();
+            return ordersOver10;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _db;
+        public UserService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Task<User> GetUser() //done
+        {
+            var userWithMaxOrder = _db.Users.OrderByDescending(o => o.Orders.Sum(q => q.Quantity)).FirstOrDefaultAsync();
+            return userWithMaxOrder;
+            
+        }
+
+
+        public Task<List<User>> GetUsers() //done
+        {
+            var inactiveUsers = _db.Users.Where(s => s.Status == UserStatus.Inactive).ToListAsync();
+            return inactiveUsers;
+        }
+
+    }
+}
